4804f71f2b5d36612fd398d875a9697efe7d68c0
Girish3214 <71925088+Girish3214@users.noreply.github.com>
2022-09-22 11:32:06 +0530

    test cases

--- src/components/elements/Actor.js
+++ src/components/elements/Actor.js
@@ -1,20 +1,24 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import React from 'react'
-import NoImage from "../images/no_image.jpg"
-import {  IMAGE_BASE_URL,  POSTER_SIZE } from "../../config";
-import { StyledActor } from "../styles/StyledActor"
+import React from "react";
+import NoImage from "../images/no_image.jpg";
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../../config";
+import { StyledActor } from "../styles/StyledActor";
 
 const Actor = ({ actor }) => {
-    return (
-        <StyledActor>
-            <img src={
-                actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}` : NoImage
-            }
-            alt="Actor Image" />
-            <span className="actor-name">{actor.name}­</span>
-            <span className="actor-character">{actor.character}</span>
-        </StyledActor>
-    )
-}
+  return (
+    <StyledActor>
+      <img
+        src={
+          actor?.profile_path
+            ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+            : NoImage
+        }
+        alt="Actor Image"
+      />
+      <span className="actor-name">{actor?.name}­</span>
+      <span className="actor-character">{actor?.character}</span>
+    </StyledActor>
+  );
+};
 
-export default Actor
+export default Actor;
